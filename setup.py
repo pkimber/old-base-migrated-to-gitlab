e681f10d2bc4257e87de74536c0841695de46ace
Patrick Kimber <code@pkimber.net>
2013-09-09 11:41:27 +0100

    version 0.0.18

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'templates/*.*',
         ],
     },
-    version='0.0.17',
+    version='0.0.18',
     description='Base',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
