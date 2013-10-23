3cf817cbed220f6d0a17287d6083d9865a7b52ff
Patrick Kimber <code@pkimber.net>
2013-10-23 12:10:21 +0100

    Tidy test utils

--- base/tests/test_utils.py
+++ base/tests/test_utils.py
@@ -9,22 +9,29 @@ from login.tests.scenario import (
 class PermTestCase(TestCase):
 
     def assert_any(self, url):
+        """all users (logged in or not) should have access to this url"""
         self.client.logout()
         response = self.client.get(url)
         self.assertEqual(
             response.status_code,
             200,
-            "status {}: everyone should not have access to '{}'".format(
+            "status {}: everyone should have access to '{}'".format(
                 response.status_code, url
             )
         )
 
+    def assert_logged_in(self, url):
+        self._assert_no_perm(url)
+        self._assert_web(url)
+
     def assert_staff_only(self, url):
+        """only members of staff should have access to this URL"""
         self._assert_no_perm(url)
         self._assert_no_web(url)
         self._assert_staff(url)
 
     def _assert_no_perm(self, url):
+        """a user who is not logged in should not have access to this URL"""
         self.client.logout()
         response = self.client.get(url)
         self.assertEqual(
@@ -37,17 +44,15 @@ class PermTestCase(TestCase):
         )
 
     def _assert_no_web(self, url):
-        web = get_user_web()
-        self.client.login(
-            username=web.username, password=web.username
-        )
+        """a user who is logged in should not have access to this url"""
+        user = self._login_web()
         response = self.client.get(url)
         self.assertEqual(
             response.status_code,
             302,
             "status {}: user '{}' should not have access "
             "to this url: '{}'".format(
-                response.status_code, web.username, url
+                response.status_code, user.username, url
             )
         )
 
@@ -65,3 +70,23 @@ class PermTestCase(TestCase):
                 response.status_code, staff.username, url
             )
         )
+
+    def _assert_web(self, url):
+        """check that a logged in user can view the url"""
+        user = self._login_web()
+        response = self.client.get(url)
+        self.assertEqual(
+            response.status_code,
+            200,
+            "status {}: web user '{}' (logged in) should have access "
+            "to this url: '{}'".format(
+                response.status_code, user.username, url
+            )
+        )
+
+    def _login_web(self):
+        web = get_user_web()
+        self.client.login(
+            username=web.username, password=web.username
+        )
+        return web
