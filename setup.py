71da4e92d259477dc6e1133ca6910c80b760fa80
Patrick Kimber <code@pkimber.net>
2013-12-16 16:44:57 +0100

    version 0.0.31

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
             'templates/base/*.*',
         ],
     },
-    version='0.0.30',
+    version='0.0.31',
     description='Base',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
