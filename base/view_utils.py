e52b883f30584cabc472751db52f2adb6d9ea629
Patrick Kimber <code@pkimber.net>
2014-01-14 22:24:11 +0000

    set path to 'home' if root url

--- base/view_utils.py
+++ base/view_utils.py
@@ -2,7 +2,10 @@ class BaseMixin(object):
 
     def get_context_data(self, **kwargs):
         context = super(BaseMixin, self).get_context_data(**kwargs)
+        path = self.request.path
+        if path == '/':
+            path = 'home'
         context.update(dict(
-            path=self.request.path,
+            path=path,
         ))
         return context
