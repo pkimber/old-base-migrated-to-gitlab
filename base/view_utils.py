9f5ae4db01f02df45a21bcb55bc01bdf91633bfb
Patrick Kimber <code@pkimber.net>
2014-01-18 17:18:18 +0000

    'path' processing is in a function

--- base/view_utils.py
+++ base/view_utils.py
@@ -1,11 +1,16 @@
+def get_path(path):
+    """Path processing can be used by other views."""
+    result = path
+    if result == '/':
+        result = 'home'
+    return result
+
+
 class BaseMixin(object):
 
     def get_context_data(self, **kwargs):
         context = super(BaseMixin, self).get_context_data(**kwargs)
-        path = self.request.path
-        if path == '/':
-            path = 'home'
         context.update(dict(
-            path=path,
+            path=get_path(self.request.path),
         ))
         return context
