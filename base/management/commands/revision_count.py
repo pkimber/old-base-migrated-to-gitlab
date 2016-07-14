ea472317065563d6edc2f554d296c0017d3f1234
Malcolm Dinsmore <zebyea@gmail.com>
2016-07-14 14:19:39 +0100

    Add revision_count management command and update requirements

---
+++ base/management/commands/revision_count.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+from django.apps import apps
+# from reversion import revisions as reversion
+from reversion.models import Version
+from reversion.errors import RegistrationError
+
+
+class Command(BaseCommand):
+
+    help = "Count reversion records for each model"
+
+    def handle(self, *args, **options):
+        total_count = 0
+        print_pattern = "{:<15} {:<30s} {:>10d}"
+        prev_app = None
+        for model in sorted(
+                apps.get_models(),
+                key=lambda mod: mod.__module__ + '.' + mod.__name__):
+            app_name = model._meta.app_label
+            model_name = model.__name__
+
+            try:
+                qs = Version.objects.get_for_model(model)
+                count = qs.count()
+                total_count += count
+                if prev_app and prev_app != app_name:
+                    print()
+                print (print_pattern.format(
+                    app_name if prev_app != app_name else "",
+                    model_name, count
+                ))
+                prev_app = app_name
+            except RegistrationError:
+                # model is not registered with reversion ignore
+                pass
+        print ()
+        print (print_pattern.format("Total Records", "", total_count))
