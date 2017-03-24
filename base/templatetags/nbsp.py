2c2694d4c9ef3fdd51039b45951223708cbef3b9
Malcolm Dinsmore <code-md@kbsoftware.co.uk>
2017-03-24 21:08:47 +0000

    Add nbsp template tag

---
+++ base/templatetags/nbsp.py
@@ -0,0 +1,11 @@
+# templatetags/nbsp.py
+
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter()
+def nbsp(value):
+    return mark_safe("&nbsp;".join(str(value).split(' ')))
