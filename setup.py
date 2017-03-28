6c186c6d1e1668005ad42232a7dc2aa70214e306
Malcolm Dinsmore <code-md@kbsoftware.co.uk>
2017-03-28 18:15:53 +0100

    version 0.2.08

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
             'templates/base/*.*',
         ],
     },
-    version='0.2.07',
+    version='0.2.08',
     description='Base',
     author='Malcolm Dinsmore',
     author_email='code-md@kbsoftware.co.uk',
